a5ae3738e40aa45119126612e04ce89941dad400
LEONARDO ARRUDA FERREIRA <leonardo.ferreira258@etec.sp.gov.br>
2026-03-26 13:38:49 -0300

    feat: estrutura inicial do projeto MeuCrud

---
+++ src/screens/CardPersonal.js
@@ -0,0 +1,1 @@
+// TODO: implementar na semana 2
---
+++ src/servers/configApi.js
@@ -0,0 +1,3 @@
+// URL gerada pelo Localtunnel
+// Atualize esta URL toda vez que reiniciar o Localtunnel
+export const API_URL = "https://abcd1234.loca.lt";
